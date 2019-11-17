1f387275d3fb3d40ee134ec243410c78d79a54d9
Yuna Jeong <yoona3316@naver.com>
2019-11-17 16:35:28 +0900

    update ImageJsonCreator

--- DataLoader/ImageJsonCreator.py
+++ DataLoader/ImageJsonCreator.py
@@ -1,16 +1,23 @@
 import json
 import codecs
 # Need refactoring for general usage
+import os
 
 
 class ImageJsonCreator:
-    def __init__(self, pure_data_path, json_path):
+    def __init__(self, path_kind="Fruits", pure_data_path="pure_data", data_list_path="data_list.json"):
+        self.base_data_path = "../Data"
+        self.path_kind = path_kind
         self.pure_data_path = pure_data_path
-        self.json_path = json_path
+        self.data_list_path = data_list_path
 
     def create_json(self):
-        f = codecs.open(self.pure_data_path, 'r', 'utf-8')
-        dataList = dict()
+        pure_data_path = os.path.join(self.base_data_path, self.path_kind, self.pure_data_path)
+        if os.path.exists(pure_data_path):
+            f = codecs.open(pure_data_path, 'r', 'utf-8')
+            dataList = dict()
+        else:
+            raise FileNotFoundError
 
         id = 0
         while True:
@@ -24,12 +31,12 @@ class ImageJsonCreator:
             dataList[id] = data
             id += 1
 
-        with open(self.json_path, 'w', encoding='utf-8') as f:
-            json.dump(dataList, f, indent='\t', ensure_ascii=False)
-
-
-pure_data_path = '../Data/pure_data'
-json_path = '../Data/data_list.json'
+        data_list_path = os.path.join(self.base_data_path, self.path_kind, self.data_list_path)
+        if os.path.exists(data_list_path):
+            with open(data_list_path, 'w', encoding='utf-8') as f:
+                json.dump(dataList, f, indent='\t', ensure_ascii=False)
+        else:
+            raise FileNotFoundError
 
 if __name__=='__main__':
-    ImageJsonCreator(pure_data_path, json_path).create_json()
+    ImageJsonCreator("Fruits").create_json()
